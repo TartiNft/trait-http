91a39ba6b02b5f8056676bdd4991f4073a615356
Evan de la Cruz <evan.delacruz@gmail.com>
2023-06-30 09:40:38 -0700

    Accept metadata instead of metadata file path since caller isnt local

--- app.js
+++ app.js
@@ -10,15 +10,43 @@ app.get("/prompt_bot", (req, res) => {
     //this is almost a direct line to the CLI and highly suseptable to injection attacks.
     //do not expose to the public internet, in this form
 
+    //here is some SO code I found and modified but have not tried.
+    //It uses arguments instead of string interpliation to call command which
+    //would fix the injection conern.
+    //Also not sure if the `bot=${botMetadataFile}` part is safe
+    /**
+        const { spawn } = require('child_process');
+        // Note that the arguments are in an array, not using string interpolation
+        const promptBot = spawn(process.env.WAPP_PATH, ['PromptBot', `bot=${botMetadataFile}`, whatToDo]);
+    
+        promptBot.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        });
+    
+        promptBot.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+        });
+    
+        promptBot.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        });
+     */
+
     if (!process.env.WAPP_PATH) {
         throw new error("WAPP_PATH environment variable must be set");
     }
 
     const whatToDo = req.query.what_to_do;
-    //bot_metadatafile was created by our caller.
-    // they got the bot metadata from the blockchain, 
-    //saved to a temp file, and that path is sent to us
-    const botMetadataFile = req.query.bot_metadatafile;
+
+    //bot_metadata was gotten by our caller.
+    //they got the bot metadata from the blockchain
+    const os = require("os");
+    const fs = require("fs");
+    const crypto = require("crypto");
+    const botMetadata = req.body.bot_metadata;
+    const metadataHash = crypto.createHash('md5').update(botMetadata).digest('hex');
+    const botMetadataFile = `${os.tmpdir()}/${metadataHash}.traitbot`;
+    fs.writeFileSync(botMetadataFile, botMetadata);
 
     let stdout = execSync(`${process.env.WAPP_PATH} PromptBot bot="${botMetadataFile}" ${whatToDo}`);
     res.json({ "BotResponse": stdout.toString() });
