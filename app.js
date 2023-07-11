5f55bc01658f46b71d913f6f55a6c83aab0b97f9
Evan De La Cruz <evan.delacruz@gmail.com>
2023-07-10 17:20:23 -0700

    Fix the way we deal with getting post body in express 4.18

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ console.log("TraitAI HTTP is starting...")
 
 const express = require('express');
 const app = express();
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// const bodyParser = require('body-parser');
 const port = 3000;
 
 app.get('/', (req, res) => res.send('TraitAI HTTP IO'));
