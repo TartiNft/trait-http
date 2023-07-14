25b169370230a220bfdfb9ec7196324e1da8af17
Evan De La Cruz <evan.delacruz@gmail.com>
2023-07-14 06:27:39 -0700

    fix bug when second arg not passed to callTraitAi

--- TraitIo.js
+++ TraitIo.js
@@ -1,4 +1,9 @@
-export const callTraitAi = (traitAiFunction, argsString) => {
+const callTraitAi = (traitAiFunction, argsString) => {
   const execSync = require('child_process').execSync;
+  if (!argsString) {
+    argsString = "";
+  }
   return execSync(`${process.env.WAPP_PATH} ${traitAiFunction} ${argsString}`);
 };
+
+module.exports = { callTraitAi };
